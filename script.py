8f2c195d5bd6b8285d724f0ef7a7cb5030ed733c
gilbert baracka <gilbert@gilberts-MacBook-Air.local>
2019-06-30 14:19:42 -0600

    json

--- script.py
+++ script.py
@@ -1,4 +1,9 @@
+import urllib.request
+import json
+import textwrap
 
-a = set("im working hard to learn python")
-b = set("not really lol")
-print(a | b)
+with urllib.request.urlopen('https://newsapi.org/v2/everything?q=bitcoin&from=2019-06-30&sortBy=publishedAt&apiKey=bc1119ab6a4c450fb7230c4c765ac97f') as info:
+
+    res = info.read()
+    decod = res.decode('utf-8')
+    print(textwrap.fill(decod))
