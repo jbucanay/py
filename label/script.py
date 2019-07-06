7e7494ad6c1851cac7982841ee485d865dbe1b16
Gilbert Baracka <gilbert.baracka@gmail.com>
2019-07-06 17:59:54 -0600

    image

--- label/script.py
+++ label/script.py
@@ -4,10 +4,10 @@ from tkinter import ttk
 root = Tk()
 label = ttk.Label(root)
 img = PhotoImage(file="widget.pgm")
-
-label.config(image=img,)
+small = img.subsample(2, 2)  # low number equals bigger image
+label.config(image=small)
 label.config(text="Hi There")
 label.config(font=('lato', 25, 'bold'))
-label.config(compound='center')
+label.config(compound=RIGHT)
 label.pack()
 root.mainloop()
