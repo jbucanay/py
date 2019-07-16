82aaac4658e9825667f0eeab1d181576b519861c
Gilbert Baracka <gilbert.baracka@gmail.com>
2019-07-16 11:09:38 -0600

    arranged

--- codeChall/input.py
+++ codeChall/input.py
@@ -1,3 +1,11 @@
-x, y, z, n = int(input()), int(input()), int(input()), int(input())
-print([[a, b, c] for a in range(0, x+1) for b in range(0, y+1)
-       for c in range(0, z+1) if a + b + c != n])
+# x, y, z, n = int(input()), int(input()), int(input()), int(input())
+# print([[a, b, c] for a in range(0, x+1) for b in range(0, y+1)
+#        for c in range(0, z+1) if a + b + c != n])
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = map(int, input().split())
+#     arranged = set(list(arr))
+#     listed = list(arranged)
+
+#     print(sorted(listed)[-2])
